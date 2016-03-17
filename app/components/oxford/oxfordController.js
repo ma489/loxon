3542dd7e859014153e1be26838bde4de30a8c131
Mansour <mansour@mansourahmed.com>
2016-03-17 20:28:29 +0000

    adding initial dummy marker

--- app/components/oxford/oxfordController.js
+++ app/components/oxford/oxfordController.js
@@ -2,18 +2,34 @@
 
 angular.module('myApp.view2', ['ngRoute'])
 
-.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/oxford', {
-    templateUrl: 'components/oxford/oxfordView.html',
-    controller: 'View2Ctrl'
-  });
-}])
+    .config(['$routeProvider', function ($routeProvider) {
+        $routeProvider.when('/oxford', {
+            templateUrl: 'components/oxford/oxfordView.html',
+            controller: 'View2Ctrl'
+        });
+    }])
 
-.controller('View2Ctrl', [function($scope) {
-  var mapOptions = {
-    zoom: 14,
-    center: new google.maps.LatLng(51.7568262, -1.2281289),
-    mapTypeId: google.maps.MapTypeId.ROADMAP
-  }
-  $scope.map = new google.maps.Map(document.getElementById('map'), mapOptions);
-}]);
+    .controller('View2Ctrl', [function () {
+        var mapOptions = {
+            zoom: 14,
+            center: new google.maps.LatLng(51.7568262, -1.2281289),
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
+
+        var map = new google.maps.Map(document.getElementById('map'), mapOptions);
+
+        var myLatLng = {lat: 51.7538029, lng: -1.2625571};
+        var marker = new google.maps.Marker({
+            position: myLatLng,
+            map: map,
+            title: 'Gloucester Green',
+            label: 'G'
+        });
+        marker.setMap(map);
+
+        marker.info = new google.maps.InfoWindow({
+            content: 'Tube: 6mins <br/> X90: 8mins'
+        });
+        marker.info.open(map, marker);
+
+    }]);
