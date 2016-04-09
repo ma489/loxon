14b08fdbd45a14378023c403cc4f8f3808fd42d2
Mansour <mansour@mansourahmed.com>
2016-04-09 23:23:26 +0100

    cleanup

--- app/components/oxford/oxfordController.js
+++ app/components/oxford/oxfordController.js
@@ -11,38 +11,21 @@ angular
     .controller('oxfordController', ['$scope', '$q', 'CoachStopService', 'OXFORD_COACH_ROUTES',
         function ($scope, $q, CoachStopService, OXFORD_COACH_ROUTES) {
             $scope.map = initialiseOxfordMap();
-
             var route_ids = _.map(OXFORD_COACH_ROUTES, function(route) {return route.id});
             var stopsPromise = _.map(route_ids, function(route_id) {return CoachStopService.getStops(route_id)});
             $q.all(stopsPromise).then(function(result) {
-                //... This callback would be called when all promised would be resolved
-                    $scope.stops = _.flatten(_.map(result, function(r) {return r.data.stops}));
-                    var stopLocationsPromises = _.map($scope.stops, function(s) {return CoachStopService.getStopLocations(s)});
-                    $q.all(stopLocationsPromises).then(function(res) {
-                        var allStopLocationCandidates = _.flatten(_.map(res, function(r) {return r.data.result }));
-                        var stopIdStrings = _.map($scope.stops, function (t) { return t.stopId.toString() });
-                        var stopLocations = _.filter(allStopLocationCandidates, function (s) {
-                                                return _.contains(stopIdStrings, s.stopId)});
-                        var stopLocationsInfo = _.map(stopLocations, function (s) {
-                            var stopInfo = {};
-                            stopInfo.id = s.stopId;
-                            stopInfo.name = s.stopName;
-                            stopInfo.lat = s.lat;
-                            stopInfo.lng = s.lng;
-                            stopInfo.desc = s.LongName;
-                            return stopInfo;
-                        });
-                        //TODO filter unique stops
-                        setMarkers($scope.map, stopLocationsInfo);
-                    });
+                $scope.stops = _.flatten(_.map(result, function(r) {return r.data.stops}));
+                var stopLocationsPromises = _.map($scope.stops, function(s) {return CoachStopService.getStopLocations(s)});
+                $q.all(stopLocationsPromises).then(function(res) { processStopLocations(res, $scope); });
             });
-
         }
     ]);
 
 //TODO a separate function to update stop times periodically
 
+
 function initialiseOxfordMap() {
+    //TODO move these constants?
     const OXFORD_LATITUDE = 51.7568262;
     const OXFORD_LONGITUDE = -1.2281289;
     const MAP_ZOOM_LEVEL = 14;
@@ -56,63 +39,50 @@ function initialiseOxfordMap() {
     return new google.maps.Map(document.getElementById('oxfordMap'), mapOptions);
 }
 
-function getRouteIds(coach_routes) {
-
+function processStopLocations(res, $scope) {
+    var allStopLocationCandidates = _.flatten(_.map(res, function (r) {
+        return r.data.result
+    }));
+    var stopIdStrings = _.map($scope.stops, function (t) {
+        return t.stopId.toString()
+    });
+    var stopLocations = _.filter(allStopLocationCandidates, function (s) {
+        return _.contains(stopIdStrings, s.stopId)
+    });
+    var stopLocationsInfo = _.map(stopLocations, function (s) {
+        var stopInfo = {};
+        stopInfo.id = s.stopId;
+        stopInfo.name = s.stopName;
+        stopInfo.lat = s.lat;
+        stopInfo.lng = s.lng;
+        stopInfo.desc = s.LongName;
+        return stopInfo;
+    });
+    //TODO filter unique stops
+    //TODO filter for oxford stops
+    setMarkers($scope.map, stopLocationsInfo, $scope.mapCentre);
 }
 
-function getStops($scope, routeIds, CoachStopService) {
-    //var stops = [];
-    for (var i = 0; i < routeIds.length; i++) {
-        var routeId = routeIds[i];
-        CoachStopService.getStops(routeId)
-            .then(function (returnedStops) {
-                if (!angular.isDefined($scope.stops)) {
-                    $scope.stops = [];
-                }
-                $scope.stops = $scope.stops.concat(returnedStops.data.stops);
-            });
-    }
-    //return stops;
-}
-
-function setMarkers(map, stopLocations) {
+function setMarkers(map, stopLocations, mapCentre) {
 
     var markers = _.map(stopLocations, function(sl) {
                         return new google.maps.Marker({
                             position: {lat: parseFloat(sl.lat), lng: parseFloat(sl.lng)},
                             map: map,
-                            title: sl.desc,
+                            title: sl.name,
                             label: sl.name.charAt(0)
                         });
                     });
 
     _.map(markers, function (m) {m.setMap(map)});
 
-    //TODO cleanup this code
+    var marker_info = _.map(markers, function (m) {
+        var content = m.title + '<br/>Tube: 6mins';
+        m.info = new google.maps.InfoWindow({
+            content: content
+        });
+        m.info.open(map, m);
+        return m.info;
+    });
 
-    //var marker_gloucester_green = new google.maps.Marker({
-    //    position: {lat: 51.7538029, lng: -1.2625571},
-    //    map: map,
-    //    title: 'Gloucester Green',
-    //    label: 'G'
-    //});
-    //marker_gloucester_green.setMap(map);
-
-    //marker_gloucester_green.info = new google.maps.InfoWindow({
-    //    content: 'Tube: 6mins <br/> X90: &nbsp;&nbsp;8mins'
-    //});
-    //marker_gloucester_green.info.open(map, marker_gloucester_green);
-
-    //var marker2 = new google.maps.Marker({
-    //    position: {lat: 51.7605924, lng: -1.2112261},
-    //    map: map,
-    //    title: 'Headington Shops',
-    //    label: 'H'
-    //});
-    //marker2.setMap(map);
-    //
-    //marker2.info = new google.maps.InfoWindow({
-    //    content: 'Tube: 7mins <br/> X90: &nbsp;&nbsp;9mins'
-    //});
-    //marker2.info.open(map, marker2);
 }
